6a0ce1189d4bb74ed10af4560d72227ef07992e4
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2023-01-16 03:01:33 -0800

    ci(jest): set max concurrency to number of cpus - 1, with a minimum of 10

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,8 @@ module.exports = {
   testRunner: 'jest-circus/runner',
   // ? 24h if debugging so MMS and other tools don't choke, otherwise 1m
   testTimeout: 1000 * 60 * (process.env.VSCODE_INSPECTOR_OPTIONS ? 60 * 24 : 1),
-  maxConcurrency: require('node:os').cpus().length - 1,
+  // ? Minimum of 10 concurrent tests executed at once; maximum of cpu cores - 1
+  maxConcurrency: Math.max(require('node:os').cpus().length - 1, 10),
   verbose: false,
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   setupFilesAfterEnv: ['./test/setup.ts'],
