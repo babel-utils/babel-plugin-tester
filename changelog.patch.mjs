e7a5d8781249b042f457196dd2518831143bed57
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-06-13 22:53:51 -0700

    docs(changelog): make changelog less spicy

--- changelog.patch.mjs
+++ changelog.patch.mjs
@@ -8,5 +8,10 @@ export default [
   [
     'Minimum supported Node.js version is now 18.19.0',
     'Minimum supported Node.js version is now 20.18.0'
-  ]
+  ],
+  [
+    String.raw`Due to prettier\@3 forcing downstream adoption of their asynchronous interface, `,
+    ''
+  ],
+  [/^.*Update core-js to 3\.33.*$\n/m, '']
 ];
